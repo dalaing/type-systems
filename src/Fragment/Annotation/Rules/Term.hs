b4920cc03e0b0d131a9df5b03342baae8cfa69c0
Dave Laing <dave.laing.80@gmail.com>
2017-03-15 11:51:48 +1000

    Adds pattern matching support for type annotations.

--- src/Fragment/Annotation/Rules/Term.hs
+++ src/Fragment/Annotation/Rules/Term.hs
@@ -14,11 +14,21 @@ module Fragment.Annotation.Rules.Term (
 import Control.Lens (review, preview)
 
 import Rules.Term
+import Ast.Pattern
 import Ast.Term
 
 import Fragment.Annotation.Ast.Term
 
 type AnnotationTermContext ki ty pt tm a = AsTmAnnotation ki ty pt tm
+
+valueAnnotation :: AsTmAnnotation ki ty pt tm
+                => (Term ki ty pt tm a -> Maybe (Term ki ty pt tm a))
+                -> Term ki ty pt tm a
+                -> Maybe (Term ki ty pt tm a)
+valueAnnotation valueFn tm = do
+  (tyA, tmA) <- preview _TmAnnotation tm
+  vA <- valueFn tmA
+  return $ review _TmAnnotation (tyA, vA)
 
 stepAnnotation :: AsTmAnnotation ki ty pt tm
                => (Term ki ty pt tm a -> Maybe (Term ki ty pt tm a))
@@ -29,11 +39,22 @@ stepAnnotation stepFn tm = do
   tmA' <- stepFn tmA
   return $ review _TmAnnotation (tyA, tmA')
 
+matchAnnotation :: AsTmAnnotation ki ty pt tm
+                => (Pattern pt a -> Term ki ty pt tm a -> Maybe [Term ki ty pt tm a])
+                -> Pattern pt a
+                -> Term ki ty pt tm a
+                -> Maybe [Term ki ty pt tm a]
+matchAnnotation matchFn pt tm = do
+  (_, tmA) <- preview _TmAnnotation tm
+  matchFn pt tmA
+
 annotationEvalRules :: AnnotationTermContext ki ty pt tm a
                     => EvalInput ki ty pt tm a
 annotationEvalRules =
   EvalInput
-    [] [ EvalStep stepAnnotation ] []
+    [ ValueRecurse valueAnnotation ]
+    [ EvalStep stepAnnotation ]
+    [ MatchRecurse matchAnnotation ]
 
 annotationTermRules :: AnnotationTermContext ki ty pt tm a
                     => TermInput ki ty pt tm a
