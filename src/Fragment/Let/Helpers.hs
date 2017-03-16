ae0996f6af28f5c963be949030424f7be40e4140
Dave Laing <dave.laing.80@gmail.com>
2017-03-16 21:25:01 +1000

    Scratch work on some ideas of working with let/letrec.

--- src/Fragment/Let/Helpers.hs
+++ src/Fragment/Let/Helpers.hs
@@ -8,12 +8,15 @@ Portability : non-portable
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 module Fragment.Let.Helpers (
-    tmLet
+    tmLetRec
+  , tmLet
+  , checkLetBindings
   ) where
 
 import Data.List (elemIndex)
 
 import Bound (abstract)
+import Bound.Scope (bindings)
 import Control.Lens (review)
 import Control.Lens.Wrapped (_Unwrapped)
 
@@ -22,16 +25,47 @@ import Ast.Term
 
 import Fragment.Let.Ast.Term
 
+tmLetRec :: (Eq a, AstBound ki ty pt tm, AsTmLet ki ty pt tm)
+         => [(a, Maybe (Type ki ty a), Term ki ty pt tm a)]
+         -> Term ki ty pt tm a
+         -> Term ki ty pt tm a
+tmLetRec bs tm =
+  let
+    bs' =
+      fmap f bs
+    f (v, ty, tm') =
+      (ATmVar v, ty, abstr . review _Unwrapped $ tm')
+    abstr =
+      abstract (`elemIndex` fmap (\(x, _, _) -> x) bs')
+    ast =
+      abstr $ review _Unwrapped tm
+  in
+    review _TmLet (fmap (\(_, ty, s) -> LetBinding (fmap (review _Type) ty) s) bs', ast)
+
 tmLet :: (Eq a, AstBound ki ty pt tm, AsTmLet ki ty pt tm)
       => [(a, Maybe (Type ki ty a), Term ki ty pt tm a)]
       -> Term ki ty pt tm a
       -> Term ki ty pt tm a
-tmLet bindings tm =
+tmLet bs tm =
   let
-    bs = fmap f bindings
-    f (v, ty, tm') = (ATmVar v, ty, abstr . review _Unwrapped $ tm')
-    abstr = abstract (`elemIndex` map (\(x, _, _) -> x) bs)
+    bs' =
+      zipWith f [0..] bs
+    f i (v, ty, tm') =
+      (ATmVar v, ty, abstr i . review _Unwrapped $ tm')
+    trim i j
+      | i < j = Just j
+      | otherwise = Nothing
+    abstr i =
+      abstract (\v -> (>>= trim i) . elemIndex v . fmap (\(x, _, _) -> x) $ bs')
+    ast =
+      abstr (length bs') $ review _Unwrapped tm
   in
-    review _TmLet (fmap (\(_, ty, s) -> LetBinding (fmap (review _Type) ty) s) bs, abstr . review _Unwrapped $ tm)
+    review _TmLet (fmap (\(_, ty, s) -> LetBinding (fmap (review _Type) ty) s) bs', ast)
 
+checkLetBindings :: Foldable tm => [LetBinding ki ty pt tm a] -> Bool
+checkLetBindings bs =
+  let
+    f i (LetBinding _ ast) = all (< i) . bindings $ ast
+  in
+    and . zipWith f [0..] $ bs
 
