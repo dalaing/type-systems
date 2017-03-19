15106167299805fb92e5c1cd7ee1d15f0d6e0efd
Dave Laing <dave.laing.80@gmail.com>
2017-03-19 22:15:51 +1000

    Spins out the AST components into their own module.

---
+++ src/Ast.hs
@@ -0,0 +1,66 @@
+{-|
+Copyright   : (c) Dave Laing, 2017
+License     : BSD3
+Maintainer  : dave.laing.80@gmail.com
+Stability   : experimental
+Portability : non-portable
+-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Ast (
+    AstIn(..)
+  , AstOut(..)
+  ) where
+
+import Util.TypeList
+
+import Ast.Kind
+import Ast.Type
+import Ast.Pattern
+import Ast.Term
+
+class AstIn (k :: j) where
+  type KindList k :: [* -> *]
+  type TypeList k :: [(* -> *) -> (* -> *) -> * -> *]
+  type PatternList k :: [(* -> *) -> * -> *]
+  type TermList k :: [(* -> *) -> ((* -> *) -> (* -> *) -> * -> *) -> ((* -> *) -> * -> *) -> (* -> *) -> * -> *]
+
+instance AstIn '[] where
+  type KindList '[] = '[]
+  type TypeList '[] = '[]
+  type PatternList '[] = '[]
+  type TermList '[] = '[]
+
+instance (AstIn k, AstIn ks) => AstIn (k ': ks) where
+  type KindList (k ': ks) = Append (KindList k) (KindList ks)
+  type TypeList (k ': ks) = Append (TypeList k) (TypeList ks)
+  type PatternList (k ': ks) = Append (PatternList k) (PatternList ks)
+  type TermList (k ': ks) = Append (TermList k) (TermList ks)
+
+class AstOut (k :: j) where
+
+  type RKindF k :: * -> *
+  type RTypeF k :: ((* -> *) -> (* -> *) -> * -> *)
+  type RPatternF k :: ((* -> *) -> * -> *)
+  type RTermF k :: ((* -> *) -> ((* -> *) -> (* -> *) -> * -> *) -> ((* -> *) -> * -> *) -> (* -> *) -> * -> *)
+
+  type RKind k :: *
+  type RType k :: (* -> *)
+  type RPattern k :: (* -> *)
+  type RTerm k :: (* -> *)
+
+instance AstIn k => AstOut (k :: j) where
+  type RKindF k = KiSum (KindList k)
+  type RTypeF k = TySum (TypeList k)
+  type RPatternF k = PtSum (PatternList k)
+  type RTermF k = TmSum (TermList k)
+
+  type RKind k = Kind (RKindF k)
+  type RType k = Type (RKindF k) (RTypeF k)
+  type RPattern k = Pattern (RPatternF k)
+  type RTerm k = Term (RKindF k) (RTypeF k) (RPatternF k) (RTermF k)
--- src/Rules.hs
+++ src/Rules.hs
@@ -37,54 +37,11 @@ import Rules.Type
 import Rules.Term
 import Util.TypeList
 
-import Ast.Kind
+import Ast
 import Ast.Type
 import Ast.Error
 import Ast.Error.Common
 import Ast.Warning
-import Ast.Pattern
-import Ast.Term
-
-class AstIn (k :: j) where
-  type KindList k :: [* -> *]
-  type TypeList k :: [(* -> *) -> (* -> *) -> * -> *]
-  type PatternList k :: [(* -> *) -> * -> *]
-  type TermList k :: [(* -> *) -> ((* -> *) -> (* -> *) -> * -> *) -> ((* -> *) -> * -> *) -> (* -> *) -> * -> *]
-
-instance AstIn '[] where
-  type KindList '[] = '[]
-  type TypeList '[] = '[]
-  type PatternList '[] = '[]
-  type TermList '[] = '[]
-
-instance (AstIn k, AstIn ks) => AstIn (k ': ks) where
-  type KindList (k ': ks) = Append (KindList k) (KindList ks)
-  type TypeList (k ': ks) = Append (TypeList k) (TypeList ks)
-  type PatternList (k ': ks) = Append (PatternList k) (PatternList ks)
-  type TermList (k ': ks) = Append (TermList k) (TermList ks)
-
-class AstOut (k :: j) where
-
-  type RKindF k :: * -> *
-  type RTypeF k :: ((* -> *) -> (* -> *) -> * -> *)
-  type RPatternF k :: ((* -> *) -> * -> *)
-  type RTermF k :: ((* -> *) -> ((* -> *) -> (* -> *) -> * -> *) -> ((* -> *) -> * -> *) -> (* -> *) -> * -> *)
-
-  type RKind k :: *
-  type RType k :: (* -> *)
-  type RPattern k :: (* -> *)
-  type RTerm k :: (* -> *)
-
-instance AstIn k => AstOut (k :: j) where
-  type RKindF k = KiSum (KindList k)
-  type RTypeF k = TySum (TypeList k)
-  type RPatternF k = PtSum (PatternList k)
-  type RTermF k = TmSum (TermList k)
-
-  type RKind k = Kind (RKindF k)
-  type RType k = Type (RKindF k) (RTypeF k)
-  type RPattern k = Pattern (RPatternF k)
-  type RTerm k = Term (RKindF k) (RTypeF k) (RPatternF k) (RTermF k)
 
 class AstIn k => RulesIn (k :: j) where
   type InferKindContextSyntax e w s r (m :: * -> *) (ki :: * -> *) (ty :: (* -> *) -> (* -> *) -> * -> *) a k :: Constraint
